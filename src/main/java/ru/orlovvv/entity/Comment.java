bcf031941d6866ac8d238b052a12bdd327ed1d5d
qu4dro <33917440+qu4dro@users.noreply.github.com>
2021-03-12 15:40:52 +0800

    Comment class updated with fields and onCreate method for date

--- src/main/java/ru/orlovvv/entity/Comment.java
+++ src/main/java/ru/orlovvv/entity/Comment.java
@@ -1,4 +1,21 @@
 package ru.orlovvv.entity;
 
+import org.apache.tomcat.jni.Local;
+
+import javax.persistence.PrePersist;
+import java.time.LocalDateTime;
+
 public class Comment {
+
+    private Long id;
+    private Long userId;
+    private Post post;
+    private String username;
+    private String message;
+    private LocalDateTime createdTime;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdTime = LocalDateTime.now();
+    }
 }
