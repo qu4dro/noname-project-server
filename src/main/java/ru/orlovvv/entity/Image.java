805d0946665ee8e6642d83f22e924e5e4e57dd3b
qu4dro <33917440+qu4dro@users.noreply.github.com>
2021-03-12 15:38:22 +0800

    Image class updated with fields

--- src/main/java/ru/orlovvv/entity/Image.java
+++ src/main/java/ru/orlovvv/entity/Image.java
@@ -1,4 +1,11 @@
 package ru.orlovvv.entity;
 
 public class Image {
+
+    private Long id;
+    private Long userId;
+    private Long postId;
+    private String name;
+    private byte[] imageBytes;
+
 }
