717b4bbf430261fc464020a66186e8b8711787cc
qu4dro <33917440+qu4dro@users.noreply.github.com>
2021-03-12 15:32:09 +0800

    User class updated view fields and onCreate fun for date

--- src/main/java/ru/orlovvv/entity/User.java
+++ src/main/java/ru/orlovvv/entity/User.java
@@ -1,4 +1,30 @@
 package ru.orlovvv.entity;
 
+import ru.orlovvv.entity.enums.Role;
+
+import javax.persistence.PrePersist;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class User {
+
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private String login;
+    private String email;
+    private String password;
+    private String bio;
+    private LocalDateTime registerDate;
+
+    private Set<Role> role = new HashSet<>();
+    private List<Post> posts = new ArrayList<>();
+
+    @PrePersist
+    protected void onCreate() {
+        this.registerDate = LocalDateTime.now();
+    }
 }
