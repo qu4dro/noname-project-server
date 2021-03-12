d83e47ddde64b8c2b5e6f036bbe35fdcfcf7b547
qu4dro <33917440+qu4dro@users.noreply.github.com>
2021-03-12 13:22:48 +0800

    Initial commit

---
+++ src/main/java/ru/orlovvv/wilt/WiltServerApplication.java
@@ -0,0 +1,13 @@
+package ru.orlovvv.wilt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiltServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiltServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/orlovvv/wilt/WiltServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.orlovvv.wilt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiltServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
